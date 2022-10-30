a56985a69c6ac654ed64c24d1614ec2838383851
GabyLopez01 <109874812+GabyLopez01@users.noreply.github.com>
2022-10-29 22:34:31 -0600

    Add files via upload

---
+++ web_estudiantes_bd/src/java/modelo/Conexion.java
@@ -0,0 +1,33 @@
+package modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public Connection conexionBD;
+    //jdbc:mysql://localhost:3306/db_estudiantesumg
+    private final String puerto= "3306";
+    private final String bd= "db_estudiantes";
+    private final String urlConexion = String.format("jdbc:mysql://localhost:%s/%s?serverTimezone=UTC",puerto, bd);
+    private final String usuario = "usr_empresa";
+    private final String contra = "Empres@123";
+    private final String jdbc ="com.mysql.cj.jdbc.Driver";
+    
+    public void abrir_conexion(){
+            try{
+                Class.forName(jdbc);
+                conexionBD = DriverManager.getConnection(urlConexion,usuario,contra);               
+               // System.out.println("Conexion Exitosa");
+            }catch(ClassNotFoundException | SQLException ex){
+                    System.out.println("Error: " + ex.getMessage());
+            }
+    }
+    
+    public void cerrar_conexion(){
+        try{
+            conexionBD.close();
+        }catch(SQLException ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+}
